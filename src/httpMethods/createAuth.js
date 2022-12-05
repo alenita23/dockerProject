ab32a74185c39078d9f4a1e73b6255baa8613983
Elena Zayceva <alenita.zayceva@gmail.com>
2022-12-05 18:37:32 +0300

    fix checkindate

--- src/httpMethods/createAuth.js
+++ src/httpMethods/createAuth.js
@@ -1,5 +1,5 @@
 import {endpoint} from "../util/variables.js";
-import axios from "axios";
+import axios from "axios"
 
 export function createAuth(body, headers) {
    return axios.post(`${endpoint}/auth`, body, headers);
--- src/util/variables.js
+++ src/util/variables.js
@@ -1,4 +1,4 @@
-import {token as bookingToken} from "../test/booking.test";
+import {token as bookingToken} from "../test/booking.test.js";
 export const endpoint = "https://restful-booker.herokuapp.com";
 export const login = "admin";
 export const password = "password123";
